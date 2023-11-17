e0ce22ba039805ed5653f8c44161fe79f33c06d2
Er-Carl Abban <102951384+KharlAbban@users.noreply.github.com>
2023-11-17 23:12:35 +0000

    Finished homepage

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,11 @@
 const allSkeltons = document.querySelectorAll(".skeleton");
 
+// Remove Skeleton On load
 window.addEventListener("load", () => {
   allSkeltons.forEach(skeleton => {
     skeleton.classList.remove("skeleton");
   });
 });
+
+// Set Year For Copyright
+document.querySelector("#year").textContent = new Date().getFullYear();
