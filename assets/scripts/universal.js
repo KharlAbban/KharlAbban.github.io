74a87b412b5af8ebef65628a147c04f95e036144
KharlAbban <khvngkharl123@gmail.com>
2023-11-24 15:13:03 +0000

    Done with 99% of CSS and HTML

--- assets/scripts/universal.js
+++ assets/scripts/universal.js
@@ -1,3 +1,8 @@
+// Set Page Theme on Load
+window.addEventListener("DOMContentLoaded", () => {
+    (localStorage.getItem("theme") == "dark") ? document.body.classList.add("dark-theme"): document.body.classList.remove("dark-theme");
+});
+
 // Remove Skeleton On load
 window.addEventListener("load", () => {
     document.querySelectorAll(".skeleton").forEach(skeleton => {
@@ -7,3 +12,11 @@ window.addEventListener("load", () => {
 
 // Set Year For Copyright
 document.querySelector("#year").textContent = new Date().getFullYear();
+
+// Chnage To Dark Mode On Mode Toggle
+document.querySelector(".mode-toggle").addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
+    document.querySelector(".mode-toggle .fa-moon").classList.toggle("d-none");
+    document.querySelector(".mode-toggle .fa-sun").classList.toggle("d-none");
+    document.body.classList.contains("dark-theme") ? localStorage.setItem("theme", "dark") : localStorage.setItem("theme", "");
+});
